c7b7d43ed04ca28bf851bdb3d711ea06fd41f51a
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 02:22:05 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.ts
@@ -0,0 +1,43 @@
+
+// Creating car variable
+let car :string ='subaru';
+
+//Test No 1
+console.log("Is car === 'subaru'? I predict True. ");
+console.log(car === 'subaru');
+
+//Test No 2
+console.log("It car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+//Test No 3
+console.log("It car != 'honda'? I predict true.");
+console.log(car != 'honda');
+
+//Test No 4
+console.log("It car !== 'fort'? I predict true.");
+console.log(car !== 'fort');
+
+//Test No 5
+console.log("It car.Lower Case == 'SUBARU'? I predict true.");
+console.log(car. toLowerCase() == 'SUBARU');
+
+//Test No 6
+console.log("It car. == 'SUBARU'? I predict false.");
+console.log(car == 'SUBARU');
+
+//Test No 7
+console.log("It car. === 'SUBARU'? I predict false.");
+console.log(car === 'SUBARU');
+
+//Test No 8
+console.log("It car === 'Train'? I predict false.");
+console.log(car === 'train');
+
+//Test No 9
+console.log("It car === 'bus'? I predict false.");
+console.log(car === 'Bus');
+
+//Test No 10
+console.log("It car === 'Cycle'? I predict false.");
+console.log(car === 'Cycle');
